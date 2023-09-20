d35f53b405a7cd1b9a50ef81683ace08a9cc72bc
Strauss-175 <144643280+Strauss-175@users.noreply.github.com>
2023-09-20 09:06:58 +0700

    New JS

---
+++ Homework JS/index.js
@@ -0,0 +1,33 @@
+// dien tich hinh vuong
+
+var canhVuong = 5;
+
+console.log("dien tich hinh vuong voi canh bang", canhVuong, "la:", canhVuong*4);
+// dien tich hinh chu nhat
+
+var chieuDai = 8;
+var chieuRong = 6;
+
+console.log("dien tich hinh chu nhat voi chieu dai bang", chieuDai, "va chieu rong bang", chieuRong, "la:", chieuDai*chieuRong);
+
+// hinh tron
+
+const banKinh = 12;
+
+console.log("dien tich hinh tron voi ban kinh bang", banKinh, "la:", banKinh**2*3.14);
+// hinh thang
+
+var canhDay1 = 15;
+var canhDay2 = 12;
+var chieuCao = 13;
+
+console.log("dien tich hinh thang voi chieu dai hai canh day bang", canhDay1, canhDay2, "cung chieu cao bang", chieuCao, "la:", (canhDay1+canhDay2)/2*chieuCao);
+// hinh tru
+
+var duongKinh= 9;
+let chieu_cao = 14;
+const xungQuanh = duongKinh*chieu_cao;
+
+console.log("dien tich xung quanh hinh tru voi duong kinh bang", duongKinh, "va chieu cao bang", chieu_cao, "la:", xungQuanh);
+console.log("dien tich toan phan bang:", xungQuanh + 2*3.14*duongKinh**2/4);
+console.log("the tich bang", 3.14*duongKinh**2/4*chieu_cao);
