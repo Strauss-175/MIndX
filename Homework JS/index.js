2dd6e352e6721b4301c783e35457413935face1d
Strauss-175 <144643280+Strauss-175@users.noreply.github.com>
2023-09-24 10:15:29 +0700

    update Homework

--- Homework JS/index.js
+++ Homework JS/index.js
@@ -1,33 +1,66 @@
-// dien tich hinh vuong
-
-var canhVuong = 5;
-
-console.log("dien tich hinh vuong voi canh bang", canhVuong, "la:", canhVuong*4);
-// dien tich hinh chu nhat
-
-var chieuDai = 8;
-var chieuRong = 6;
-
-console.log("dien tich hinh chu nhat voi chieu dai bang", chieuDai, "va chieu rong bang", chieuRong, "la:", chieuDai*chieuRong);
-
-// hinh tron
-
-const banKinh = 12;
-
-console.log("dien tich hinh tron voi ban kinh bang", banKinh, "la:", banKinh**2*3.14);
-// hinh thang
-
-var canhDay1 = 15;
-var canhDay2 = 12;
-var chieuCao = 13;
-
-console.log("dien tich hinh thang voi chieu dai hai canh day bang", canhDay1, canhDay2, "cung chieu cao bang", chieuCao, "la:", (canhDay1+canhDay2)/2*chieuCao);
-// hinh tru
-
-var duongKinh= 9;
-let chieu_cao = 14;
-const xungQuanh = duongKinh*chieu_cao;
-
-console.log("dien tich xung quanh hinh tru voi duong kinh bang", duongKinh, "va chieu cao bang", chieu_cao, "la:", xungQuanh);
-console.log("dien tich toan phan bang:", xungQuanh + 2*3.14*duongKinh**2/4);
-console.log("the tich bang", 3.14*duongKinh**2/4*chieu_cao);
+// const chieuCao = parseInt(prompt("Nhap chieu cao (cm)"));
+// const canNang = parseInt(prompt("Nhap can nang (kg)"));
+// let BMI = canNang / (chieuCao/100)**2;
+// switch (true) {
+//   case BMI < 16: {
+//     console.log(`${BMI} => Gay do III`);
+//     break;
+//   }
+//   case BMI < 17: {
+//     console.log(`${BMI} => Gay do II`);
+//     break;
+//   }
+//   case BMI < 18.5: {
+//     console.log(`${BMI} => Gay do I`);
+//     break;
+//   }
+//   case BMI < 25: {
+//     console.log(`${BMI} => Binh thuong`);
+//     break;
+//   }
+//   case BMI < 30: {
+//     console.log(`${BMI} => Thua can`);
+//     break;
+//   }
+//   case BMI < 35: {
+//     console.log(`${BMI} => Beo phi do I`);
+//     break;
+//   }
+//   case BMI < 40: {
+//     console.log(`${BMI} => Beo phi II`);
+//     break;
+//   }
+//   case BMI > 40: {
+//     console.log(`${BMI} => Beo phi do III`);
+//     break;
+//   }
+//   default: {
+//     console.log("Loi, hay nhap lai...");
+//     break;
+//   }
+// }
+let x = parseInt(prompt("Nhap mot so nguyen X"));
+if (x < 2) {
+  console.log("x khong phai la so nguyen to");
+}
+{
+  for (let i = 0; i < x; i++) {
+    if (x % i == 0) {
+      console.log("x khong phai la so nguyen to");
+    } else {
+      console.log("x la so nguyen to");
+    }
+  }
+}
+{
+  let giaiThua =1
+  for (let i = 1; i <= x; i++) {
+    giaiThua *=i;
+  }
+  console.log("Giai thua x =", giaiThua);
+}
+let y = parseInt(prompt("Nhap mot so nguyen Y"));
+let N = 0;
+for (let i = 1; i < y; i++) {
+  if (i%2 == 0) {N++}}
+console.log(`Y cos ${N} so le`)
