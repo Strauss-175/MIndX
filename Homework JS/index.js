c8c0e2a560b94e336000771ca853888a39122321
Strauss-175 <144643280+Strauss-175@users.noreply.github.com>
2023-10-04 09:32:43 +0700

    homework day 10

--- Homework JS/index.js
+++ Homework JS/index.js
@@ -1,19 +1,11 @@
 function Logic() {
-  if (username == "admin" && password == "admin123") {
+  if (username.value == "admin" && password.value == "admin123") {
     alert("Right Password");
   } else {
     alert("Wrong UserName or Password");
   }
 }
-var ary = [];
-function user() {
-  for (var i = 0; i < username.length; i++) {
-    if (username[i].type.toLowerCase() === "admin") {
-      ary.push(username[i]);
-    }
-  }
-}
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const btn = document.getElementById("btn");
-btn.onclick = console.log(ary);
+btn.onclick = Logic
