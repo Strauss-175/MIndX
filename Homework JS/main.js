18c0ef61eea1c82392fb202030cc548b6581339d
Strauss-175 <144643280+Strauss-175@users.noreply.github.com>
2024-02-24 12:48:03 +0700

    z

--- Homework JS/main.js
+++
@@ -1,5 +0,0 @@
-const obj1 = [1, 2, 3]
-const obj2 = [4, 5, 6]
-import { ary } from "./sum.js";
-ary = [...obj1, ...obj2]
-console.log(output)
--- Homework JS/sum.js
+++
@@ -1,7 +0,0 @@
-function sum() {
-  ary = [];
-  for (let i = 0; i < ary.length; i++) {
-    let output = ary[i];
-    output += ary[i];
-  }
-}
